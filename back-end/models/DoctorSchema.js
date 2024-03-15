43d644b536540e5b197e321642472e993521f19b
congbang0711 <bangnguyen.071102@gmail.com>
2024-03-15 18:11:42 +0700

    Modify some schemas

--- back-end/models/DoctorSchema.js
+++ back-end/models/DoctorSchema.js
@@ -1,33 +1,30 @@
 import mongoose from 'mongoose';
 
-const bookingSchema = new mongoose.Schema(
-    {
-        doctor: {
-            type: mongoose.Types.ObjectId,
-            ref: 'Doctor',
-            required: true,
-        },
-        user: {
-            type: mongoose.Types.ObjectId,
-            ref: 'User',
-            required: true,
-        },
-        ticketPrice: { type: String, required: true },
-        appointmentDate: {
-            type: Date,
-            required: true,
-        },
-        status: {
-            type: String,
-            enum: ['pending', 'approved', 'cancelled'],
-            default: 'pending',
-        },
-        isPaid: {
-            type: Boolean,
-            default: true,
-        },
+const doctorSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    fullname: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    phone: { type: Number },
+    photo: { type: String },
+    ticketPrice: { type: Number },
+    role: { type: String },
+    specialization: { type: String },
+    qualifications: { type: Array },
+    experiences: { type: Array },
+    bio: { type: String, maxLength: 50 },
+    about: { type: String },
+    timeSlots: { type: Array },
+    reviews: [{ type: mongoose.Types.ObjectId, ref: 'Review' }],
+    averageRating: { type: Number, default: 0 },
+    totalRating: { type: Number, default: 0 },
+    isApproved: {
+        type: String,
+        enum: ['pending', 'approved', 'cancelled'],
+        default: 'pending',
     },
-    { timestamps: true },
-);
+    appointments: [{ type: mongoose.Types.ObjectId, ref: 'Appointment' }],
+});
 
-export default mongoose.model('Booking', bookingSchema);
+export default mongoose.model('Doctors', doctorSchema);
