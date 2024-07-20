55b10526e1516147199c0223c2985287d478c8fb
congbang0711 <bangnguyen.071102@gmail.com>
2024-07-21 00:42:08 +0700

    fix: get user info in back-end

--- back-end/Routes/user.js
+++ back-end/Routes/user.js
@@ -12,7 +12,7 @@ import { authenticate, restrict } from './../auth/verifyToken.js';
 
 const router = express.Router();
 
-router.get('/:id', authenticate, restrict(['patient']), getSingleUser);
+router.get('/:id', getSingleUser);
 router.get('/', authenticate, restrict(['admin']), getAllUsers);
 router.put('/:id', authenticate, restrict(['patient']), updateUser);
 router.delete('/:id', authenticate, restrict(['patient']), deleteUser);
